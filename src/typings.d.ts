2a0218e270a1f88aae7ced25848b3473b03d09da
nasa.wang <nasa127@gmail.com>
2018-08-15 11:46:35 +0800

    加入live reload

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,4 +2,5 @@ declare module '*.json' {
   const value: any;
   export default value;
 }
+declare const ENV: string;
 declare module 'history/createHashHistory';
--- tools/rollup.config.js
+++ tools/rollup.config.js
@@ -1,5 +1,10 @@
 const { inputOptions, outputOptions } = require('./config');
 let rollupConfig = {};
-rollupConfig = Object.assign(rollupConfig, inputOptions('dist'));
-rollupConfig.output = outputOptions('dist');
+
+console.log(process.env.NODE_ENV);
+
+const type = process.env.NODE_ENV == 'production' ? 'dist' : 'dev';
+
+rollupConfig = Object.assign(rollupConfig, inputOptions(type));
+rollupConfig.output = outputOptions(type);
 export default [rollupConfig];
